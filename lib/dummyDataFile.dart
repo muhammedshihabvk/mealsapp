ab457e6ed83fcc0bf501c3a7190eda0e56779456
LITMUS7\muhammed.shihab <shihabvk99@gmail.com>
2021-02-12 21:39:00 +0530

    Initial commit

---
+++ lib/dummyDataFile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:mealsapp/category.dart';
+
+const dummyDate = const [
+  Category(id: 'c1', title: 'Indian', color: Colors.green),
+  Category(id: 'c2', title: 'Chaines', color: Colors.green),
+  Category(id: 'c3', title: 'Western', color: Colors.green),
+  Category(id: 'c4', title: 'French', color: Colors.green),
+  Category(id: 'c5', title: 'continental', color: Colors.green),
+  Category(id: 'c6', title: 'Quick and easy', color: Colors.green),
+  Category(id: 'c7', title: 'German', color: Colors.green),
+  Category(id: 'c8', title: 'Canada', color: Colors.green),
+  Category(id: 'c9', title: 'Italian', color: Colors.green),
+];
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals App"),
+      ),
+      body: Container(
+        child: CategoriesScreen(),
+      ),
+    );
+  }
+}
+
+
